238e91b2d806b68705a7776b7d6ce76d1a7e1c10
Alex Dresko <me@alexdresko.com>
2014-10-29 23:47:06 +0000

    Formatted 'latest from github' results

--- scripts/index.js
+++ scripts/index.js
@@ -5,15 +5,28 @@ var deLaAlex;
             var _this = this;
             this.githubEvents = ko.observableArray([]);
             $.getJSON("https://api.github.com/repos/alexdresko/De-la-Alex/events").done(function (data) {
+                var count = 0;
                 $.each(data, function (i1, v1) {
                     if (v1.type == "PushEvent") {
-                        _this.addEvent("Push", v1.actor.login, v1.type, v1.payload.commits[0].message, v1.created_at);
+                        $.each(v1.payload.commits, function (i2, v2) {
+                            _this.addEvent("Push", v1.actor.login, v1.type, v2.message, v1.created_at, v2.url);
+                        });
+                    }
+
+                    if (v1.type == "IssuesEvent") {
+                        _this.addEvent("Issue " + v1.payload.action, v1.actor.login, v1.type, v1.payload.issue.title, v1.created_at, v1.payload.issue.url);
+                    }
+
+                    count++;
+
+                    if (count == 8) {
+                        return false;
                     }
                 });
             });
         }
-        Index.prototype.addEvent = function (title, author, type, subject, date) {
-            this.githubEvents.push({ "title": title, "author": author, "type": type, "subject": subject, "date": date });
+        Index.prototype.addEvent = function (title, author, type, subject, date, url) {
+            this.githubEvents.push({ "title": title, "author": author, "type": type, "subject": subject, "date": date, "url": url.replace('/api.', '/').replace('/repos', '') });
         };
         return Index;
     })();
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,17 +1,29 @@
 /// <reference path="../typings/jquery/jquery.d.ts" />
 /// <reference path="../typings/knockout/knockout.d.ts" />
-/// <reference path="../typings/knockout/knockout.d.ts" />
 
 module deLaAlex  {
     export class Index {
         constructor() {
             $.getJSON("https://api.github.com/repos/alexdresko/De-la-Alex/events")
                 .done((data) => {
+                    var count = 0;
                     $.each(data, (i1, v1) => {
                         if (v1.type == "PushEvent") {
-                            this.addEvent("Push", v1.actor.login, v1.type, v1.payload.commits[0].message, v1.created_at)
+                            $.each(v1.payload.commits, (i2, v2) => {
+                                this.addEvent("Push", v1.actor.login, v1.type, v2.message, v1.created_at, v2.url)
+                            })
+                            
                         }
                         
+                        if (v1.type == "IssuesEvent") {
+                            this.addEvent("Issue " + v1.payload.action, v1.actor.login, v1.type, v1.payload.issue.title, v1.created_at, v1.payload.issue.url);
+                        }
+                        
+                        count++;
+                        
+                        if (count == 8) {
+                            return false;
+                        }
                         
                     })
                 })
@@ -19,20 +31,22 @@ module deLaAlex  {
         
         githubEvents = ko.observableArray([]);
         
-        addEvent(title: string, author: string, type: string, subject: string, date: Date) {
-            this.githubEvents.push({ "title": title, "author": author, "type": type, "subject": subject, "date": date});
+        addEvent(title: string, author: string, type: string, subject: string, date: Date, url: string) {
+            this.githubEvents.push({ "title": title, "author": author, "type": type, "subject": subject, "date": date, "url": url.replace('/api.', '/').replace('/repos', '')});
         }
     }
 }
 
 $(function() {
     var data = new deLaAlex.Index();
+
+    
     // extend your view-model with pager.js specific data
-    (<any>pager).extendWithPage(data);
+    pager.extendWithPage(data);
     // apply the view-model using KnockoutJS as normal
     ko.applyBindings(data);
     // start pager.js
-    (<any>pager).start();
+    pager.start();
 
 })
 
